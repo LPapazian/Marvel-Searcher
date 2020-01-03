f242070ccb19a1caf54d2cbec70657a73fa5aeb6
lilapapazian <lila.papazian@despegar.com>
2020-01-02 21:19:22 -0300

    Error page: styles and component

---
+++ src/components/ErrorPage/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Errorpage.module.scss";
+
+const ErrorPage = () => {
+  return <div className={styles.errorPage}>Something went wrong</div>;
+};
+
+export default ErrorPage;
